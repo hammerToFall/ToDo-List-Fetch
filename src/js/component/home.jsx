086cff201f6b237d069e5a9191f8a503dbd97ae5
hammerToFall <lenorka0@hotmail.com>
2022-07-12 16:57:59 +0000

    subo los cambios

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ToDoList from "./todolist.jsx";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-			<ToDoList/>
+		<div className="containeer">
+			<div className="row justify-content-center">
+				<h1 className="text-center my-4">ToDo List</h1>
+				<ToDoList />
+			</div>
 		</div>
 	);
 };
--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,7 +1,6 @@
 import { element } from "prop-types";
 import React, { useState, useEffect } from "react";
 
-let aux = [];
 const url = "https://assets.breatheco.de/apis/fake/todos/user/elenak1345";
 
 const ToDoList = () => {
@@ -15,7 +14,6 @@ const ToDoList = () => {
     getList();
   }, []);
 
-  // creamos usuario
   const createUser = async () => {
     await fetch(url, {
       method: "POST",
@@ -29,17 +27,19 @@ const ToDoList = () => {
           throw new Error("Usuario ya creado anteriormente"); //creamos un objeto de tipo Error
         return resp.json();
       })
-      .then((data) => {
-        getList();
-      })
+      .then((data) => {getList();})
       .catch((error) => {
-        //imprimimos el mensaje del error
         console.log(error);
       });
   };
 
   const getList = async () => {
-    await fetch(url) //descargamos la lista
+    await fetch(url, {
+      method: "GET",      //descargamos la lista
+      headers: {
+          "Content-type": "application/json",
+      },
+  }) 
       .then(
         (resp) => resp.json() //si no ha habido error decodificamos el codigo
       )
@@ -59,10 +59,7 @@ const ToDoList = () => {
 
   const addTask = () => {
     if (text.length > 0) {
-      let taskObj = {
-        label: text,
-        done: false,
-      };
+      let taskObj = {label: text, done: false,};
       setTask([...task, taskObj]);
       console.log(task);
       setText("");
@@ -78,13 +75,7 @@ const ToDoList = () => {
           <div>
             <button
               className="delete-button btn"
-              onClick={() => {
-                let aux = task.filter(
-                  (element, index) => element.id != item.id
-                );
-                setTask([...aux]);
-              }}
-            >
+              onClick={()=> deleteIndex(index)}>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
@@ -102,10 +93,19 @@ const ToDoList = () => {
     ));
   };
 
-  const uploadList = async () =>{
+  const deleteIndex = (delId) => {
+    let aux = task.filter(
+        (element, index) => index != delId
+    );
+      setTask([...aux])
+    
+    uploadList(aux)
+  }
+
+  const uploadList = async (tareas) =>{
     await fetch(url,{
       method: "PUT",
-      body: JSON.stringify(task),
+      body: JSON.stringify(tareas),
       headers: {
         "Content-Type": "application/json"
       }
@@ -116,37 +116,37 @@ const ToDoList = () => {
       return response.json()             // new: para crear/instanciar objetos de una clase(se llama el constructor de una clase)
     })
     .then(data=>{
-      // getList()
+       getList()
     })
     .catch(error=>{
       console.log('error')
       createUser();
       uploadList();
     })
-
   }
-  // const renderList = (item, index) => {
-  //     return (
-  //         <div key={index}>
-  //             <li className="list-group-item d-flex py-2 rounded">
-  //                 <div className="d-flex w-100">{item.label}</div>
-  //                 <div>
-  //                     <button className="delete-button btn" onClick={()=> {
-  //                         let aux = task.filter((element,index) => element.id != item.id)
-  //                         setTask([...aux])
-  //                     }}>
-  //                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
-  //                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
-  //                         </svg>
-  //                     </button>
-  //                 </div>
-  //             </li>
-  //         </div>)
-  // }
+
+  const deleteAllTasks = () =>{
+    fetch(url,{
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    .then(response=>{
+      if(!response.ok)
+        throw new Error("Delete failed")
+        return response.json()
+    })
+    .then(data => {
+      createUser()
+      alert('Lista eliminada')
+    })
+    .catch(error => alert(error))
+  }
+
   return (
-    <div className="col-9">
+    <div className="col-7">
       <div className="mb-5 d-flex">
-        {/* <label htmlFor='textInput' className="form-label"></label> */}
         <input
           type="text"
           value={text}
@@ -156,17 +156,19 @@ const ToDoList = () => {
           onChange={handleText}
           onKeyUp={(e) => e.keyCode == 13 && addTask()}
         />
-        {/* <button className='btn btn-outline-danger d-flex' onClick={addTask}>Buscar</button> */}
       </div>
-      {/* {task.map(renderList)} */}
       {task.length != 0 ? (
         <div className="label">
           <label>{task.length} item left</label>
         </div>
-      ) : (
+        ) : (
         <div className="no-task">No pending task</div>
-        )}
-        {loader(task)}
+      )}
+      {/* {task.map(renderList)} */}
+      {loader(task)}
+      <div className="deleteTasks">
+        <button className="btn btn-warning" onClick={() => {deleteAllTasks()}}>Delete all</button>
+      </div>
     </div>
   );
 };
